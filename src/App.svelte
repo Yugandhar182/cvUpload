da4a74db7a6c1b4824905057aa573e9b7978a896
Yugandhar182 <yugandhar@recruitly.io>
2023-06-02 16:31:20 +0530

    files added

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
 	import { onMount } from "svelte";
 	import { createEventDispatcher } from "svelte";
-	import 'bootstrap/dist/css/bootstrap.min.css';
+	import Table from "./Table.svelte";
+	import Popup from "./Popup.svelte";
+	import SuccessPopup from "./SuccessPopup.svelte";
+	import DeletePopup from "./DeletePopup.svelte";
+	import EditPopup from "./EditPopup.svelte";
   
 	let jsonData = [];
 	let tableVisible = false;
@@ -13,8 +17,7 @@
 	let isDeletePopupVisible = false;
 	let editJob = null;
 	let isEditPopupVisible = false;
-	let isUploadSuccess = false; // New state variable for success message
-  
+	let isUploadSuccess = false;
 	const dispatch = createEventDispatcher();
   
 	onMount(async () => {
@@ -23,15 +26,14 @@
 	});
   
 	async function fetchData() {
-	  const response = await fetch("https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E");
+	  const response = await fetch(
+		"https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E"
+	  );
 	  const responseData = await response.json();
 	  jsonData = responseData.data;
 	}
   
-	function handleTitleClick(job) {
-	  selectedJob = job;
-	  dispatch("showPopup");
-	}
+	
   
 	function handleUploadCV(jobId) {
 	  uploadJobId = jobId;
@@ -46,28 +48,29 @@
 	  const formData = new FormData();
 	  formData.append("file", file);
   
-	  fetch(`https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E&candidateId=${uploadJobId}`, {
-		method: "POST",
-		body: formData
-	  })
-		.then(response => {
+	  fetch(
+		`https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E&candidateId=${uploadJobId}`,
+		{
+		  method: "POST",
+		  body: formData,
+		}
+	  )
+		.then((response) => {
 		  // Handle the response accordingly
 		  if (response.ok) {
 			console.log("CV uploaded successfully!");
-			isUploadSuccess = true; // Set the flag to true
+			isUploadSuccess = true;
 		  } else {
 			console.error("CV upload failed.");
 		  }
 		})
-		.catch(error => {
+		.catch((error) => {
 		  console.error("CV upload error:", error);
 		})
 		.finally(() => {
-		  // Perform close logic
 		  isPopupVisible = false;
 		});
 	}
-  
 	function handleSave() {
 	  // Perform save logic
 	  // In this case, we're updating the backend API URL with the file upload
@@ -97,8 +100,10 @@
 	}
   
 	function handleClose() {
-	  // Perform close logic
 	  isPopupVisible = false;
+	  isDeletePopupVisible = false;
+    isEditPopupVisible = false;
+	 isUploadSuccess=false;
 	}
   
 	function handleDelete(jobId) {
@@ -107,10 +112,13 @@
 	}
   
 	function handleDeleteConfirm() {
-	  fetch(`https://api.recruitly.io/api/candidate/${deleteJobId}?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`, {
-		method: "DELETE"
-	  })
-		.then(response => {
+	  fetch(
+		`https://api.recruitly.io/api/candidate/${deleteJobId}?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`,
+		{
+		  method: "DELETE",
+		}
+	  )
+		.then((response) => {
 		  // Handle the response accordingly
 		  if (response.ok) {
 			console.log("Job deleted successfully!");
@@ -120,7 +128,7 @@
 			console.error("Job delete failed.");
 		  }
 		})
-		.catch(error => {
+		.catch((error) => {
 		  console.error("Job delete error:", error);
 		})
 		.finally(() => {
@@ -134,16 +142,14 @@
 	}
   
 	function handleEditSave() {
-	  // Perform save logic for the edited job
-	  // Update the backend API URL with the updated job details
-	  fetch(`https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`, {
+	  fetch("https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E", {
 		method: "POST",
 		body: JSON.stringify(editJob),
 		headers: {
-		  "Content-Type": "application/json"
-		}
+		  "Content-Type": "application/json",
+		},
 	  })
-		.then(response => {
+		.then((response) => {
 		  // Handle the response accordingly
 		  if (response.ok) {
 			console.log("Job edited successfully!");
@@ -153,7 +159,7 @@
 			console.error("Job edit failed.");
 		  }
 		})
-		.catch(error => {
+		.catch((error) => {
 		  console.error("Job edit error:", error);
 		})
 		.finally(() => {
@@ -163,89 +169,27 @@
   </script>
   
   <main class="container mt-4">
-	{#if tableVisible}
-	  <table class="table">
-		<thead class="thead-light">
-		  <tr>
-			<th>ID</th>
-			<th>firstName</th>
-			<th>surname</th>
-			<th>email</th>
-			<th>mobile</th>
-			<th>Actions</th>
-		  </tr>
-		</thead>
-		<tbody>
-		  {#each jsonData as job}
-			<tr>
-			  <td>{job.id}</td>
-			  <td>{job.firstName}</td>
-			  <td>{job.surname}</td>
-			  <td>{job.email}</td>
-			  <td>{job.mobile}</td>
-			  <td>
-				<button on:click={() => handleUploadCV(job.id)} class="btn btn-primary">Upload CV</button>
-				<button on:click={() => handleEdit(job)} class="btn btn-info">Edit</button>
-				<button on:click={() => handleDelete(job.id)} class="btn btn-danger">Delete</button>
-			  </td>
-			</tr>
-		  {/each}
-		</tbody>
-	  </table>
-	  {#if isPopupVisible && uploadJobId !== null}
-		<div class="popup">
-		  <h4>Upload CV</h4>
-		  <input type="file" on:change={handleFileUpload} />
-		  <button on:click={handleSave} class="btn btn-primary">Save</button>
-		  <button on:click={handleClose} class="btn btn-secondary">Close</button>
-		</div>
-	  {/if}
-	  {#if isUploadSuccess}
-		<div class="popup success">
-		  <h4>CV Uploaded Successfully!</h4>
-		  <button on:click={() => isUploadSuccess = false} class="btn btn-primary">OK</button>
-		</div>
-	  {/if}
-	  {#if isDeletePopupVisible && deleteJobId !== null}
-		<div class="popup">
-		  <h4>Confirm Delete</h4>
-		  <p>Are you sure you want to delete this job?</p>
-		  <button on:click={handleDeleteConfirm} class="btn btn-danger">Delete</button>
-		  <button on:click={() => isDeletePopupVisible = false} class="btn btn-secondary">Cancel</button>
-		</div>
-	  {/if}
-	  {#if isEditPopupVisible && editJob !== null}
-		<div class="popup">
-		  <h4>Edit Job</h4>
-		  <label for="firstName">First Name:</label>
-		  <input type="text" id="firstName" bind:value={editJob.firstName} class="form-control" />
-		  <label for="surname">Surname:</label>
-		  <input type="text" id="surname" bind:value={editJob.surname} class="form-control" />
-		  <label for="email">Email:</label>
-		  <input type="email" id="email" bind:value={editJob.email} class="form-control" />
-		  <label for="mobile">Mobile:</label>
-		  <input type="text" id="mobile" bind:value={editJob.mobile} class="form-control" />
-		  <button on:click={handleEditSave} class="btn btn-primary">Save</button>
-		  <button on:click={() => isEditPopupVisible = false} class="btn btn-secondary">Cancel</button>
-		</div>
-	  {/if}
+	<Table {jsonData} {handleUploadCV} {handleDelete} {handleEdit} />
+	{#if isPopupVisible && uploadJobId !== null}
+	  <Popup {handleFileUpload} {handleClose} />
+	{/if}
+	{#if isUploadSuccess}
+	  <SuccessPopup on:dismiss={handleClose} />
+	{/if}
+	{#if isDeletePopupVisible && deleteJobId !== null}
+	  <DeletePopup
+		{handleDeleteConfirm}
+		{handleClose}
+	  />
+	{/if}
+	{#if isEditPopupVisible && editJob !== null}
+	  <EditPopup
+		{editJob}
+		{handleEditSave}
+		{handleClose}
+	  />
 	{/if}
   </main>
   
-  <style>
-	.popup {
-	  position: fixed;
-	  top: 50%;
-	  left: 50%;
-	  transform: translate(-50%, -50%);
-	  background-color: white;
-	  padding: 20px;
-	  border-radius: 8px;
-	  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-	}
-  
-	.success {
-	  background-color: lightgreen;
-	}
-  </style>
+
   
---
+++ src/DeletePopup.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let handleDeleteConfirm;
+    export let handleClose;
+  </script>
+  
+  <style>
+    @import 'bootstrap/dist/css/bootstrap.min.css';
+  
+    .popup {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: white;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+  </style>
+  
+  <div class="popup">
+    <h4>Confirm Delete</h4>
+    <p>Are you sure you want to delete this job?</p>
+    <button on:click={handleDeleteConfirm} class="btn btn-danger">Delete</button>
+    <button on:click={handleClose} class="btn btn-secondary">Cancel</button>
+  </div>
+  
